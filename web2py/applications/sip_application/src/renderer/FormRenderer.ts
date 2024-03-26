caeef819eed500fc7cbee245efe568396aba96b1
jegomezV <jegomezvalencia@gmail.com>
2024-03-26 13:24:44 -0500

    First change implement student model to FormRender

--- web2py/applications/sip_application/src/renderer/FormRenderer.ts
+++ web2py/applications/sip_application/src/renderer/FormRenderer.ts
@@ -3,16 +3,17 @@ import { StudentModel } from '../models/StudentModel';
 export default class FormRenderer {
 
     render(students: StudentModel[] = []): string {
-
-        return `
+        let studentForms = students.map(student => `
             <!-- Student registration form -->
             <form id="student-registration-form">
                 <label for="name">Name:</label><br>
-                <input type="text" id="name" name="name"><br>
+                <input type="text" id="name" name="name" value="${student.name}"><br>
                 <label for="email">Email:</label><br>
-                <input type="text" id="email" name="email"><br>
+                <input type="text" id="email" name="email" value="${student.email}"><br>
                 <input type="submit" value="Register">
             </form>
-        `;
+        `).join('');
+
+        return studentForms;
     }
 }
