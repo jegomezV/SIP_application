790d4ee2e3793a2ded128c3da63d8a7ec3ff193b
jegomezV <jegomezvalencia@gmail.com>
2024-03-28 18:49:19 -0500

    Last changes, app deployed

--- web2py/applications/sip_application/migrations/alembic/versions/611c77106a95_tables_create.py
+++ web2py/applications/sip_application/migrations/alembic/versions/611c77106a95_tables_create.py
@@ -1,8 +1,8 @@
-"""Tables create
+"""tables_create
 
-Revision ID: 43247ca11e39
+Revision ID: 611c77106a95
 Revises: 
-Create Date: 2024-03-25 22:04:21.515756
+Create Date: 2024-03-28 16:29:15.351142
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '43247ca11e39'
+revision: str = '611c77106a95'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
---
+++ web2py/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Wait for Postgres to become available
+until PGPASSWORD="$POSTGRES_PASSWORD" psql -h db -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+    sleep 1
+done
+
+# Cambia al directorio de alembic y ejecuta las migraciones
+cd /app/applications/sip_application/migrations
+alembic upgrade head
+
+# Cambia al directorio de web2py e inicia la aplicación
+cd /app
+echo "Join app"
+exec python3 web2py.py -a 'root' -c cert.pem -k key.pem -i 0.0.0.0
